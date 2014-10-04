a53f205a2106e90834821f972165cc8451678e65
Jonathan Tau <jtau@berkeley.edu>
2014-10-03 23:56:08 -0700

    Initial Commit

---
+++ Pikachoose/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Pikachoose
+//
+//  Created by Jonathan Tau on 10/3/14.
+//  Copyright (c) 2014 Pikachoose Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
