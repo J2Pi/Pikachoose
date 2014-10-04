6913b55805eee757d2f6bd83a812a4077812da5f
Jonathan Tau <jtau@berkeley.edu>
2014-10-04 01:47:10 -0700

    Added all the tabs and the view controllers

---
+++ Pikachoose/ThirdViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ThirdViewController.swift
+//  Pikachoose
+//
+//  Created by Jonathan Tau on 10/4/14.
+//  Copyright (c) 2014 Pikachoose Inc. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+}
+
+
